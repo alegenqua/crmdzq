6f2cdf9f50ee95e5f310a772cfa403763c3de8e4
Tognozzi <gabrio.tognozzi@moveax.it>
2021-07-22 12:35:04 +0200

    integrazione con mailgun

--- src/app/user/user.router.ts
+++ src/app/user/user.router.ts
@@ -1,9 +1,30 @@
+import Mailgun from 'mailgun-js';
 import { Body, Controller, Delete, Get, Post, Put, Query, Route, Tags } from 'tsoa';
 import { getAllUser, createUser, updateUser, deleteUser } from './user.service';
 
 @Tags('User Permission')
-@Route('/api/user-permission')
+@Route('/api/users')
 export class UserPermissionController extends Controller {
+
+
+  @Post('/signUp/')
+  public async signUp(@Body() body: {email: string, password:string, username: string}){
+    
+    const mailgun = new Mailgun({apiKey: process.env.MAILGUN_API_KEY!, 
+      domain:"sandbox9115bd93ad154fbc87f655ec4eaad651.mailgun.org"});
+
+    const data = {
+        from: "admin@crmdzq.it",
+        to: body.email,
+        subject: "Registrazione CRMDZQ",
+        text:"Benvenuto in CRMDZQ, il tuo token è TODO"
+    }
+    const result = await mailgun.messages().send(data);
+
+    return result
+  }
+
+
 
   @Get('/get-all/')
   public async getAllUser() {
@@ -24,6 +45,8 @@ export class UserPermissionController extends Controller {
   public async deleteUser(@Query('id') id: string) {
     return deleteUser({ id: Number(id) });
   }
+
+
 
 }
 
